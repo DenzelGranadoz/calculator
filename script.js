14031dede498f1a96fe6b6fa2adad230e56182e7
Denzel Granadoz <zelgranadoz@gmail.com>
2021-08-07 07:09:55 +0800

    append button values without errors

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
 //selectors
 const previousItem = document.getElementById("previous-item");
 const currentItem = document.getElementById("current-item");
-const allButtons = document.querySelectorAll(".btn");
+const allClearButton = document.querySelector(".all-clear-btn");
+const equalButton = document.querySelector(".equal-btn");
+const deleteButton = document.querySelector(".delete-btn");
+const numberButtons = document.querySelectorAll(".number-btn");
+const operatorButtons = document.querySelectorAll(".operator-btn");
 
 //event listeners
-allButtons.forEach((button) => {
-    button.addEventListener("click", buttonEvents);
-});
-
+allClearButton.addEventListener("click", allClear);
+equalButton.addEventListener("click", equal);
+deleteButton.addEventListener("click", deleteBtn);
+numberButtons.forEach((number) => number.addEventListener("click", numbers));
+operatorButtons.forEach((operator) => operator.addEventListener("click", operators));
 
 //functions
-function buttonEvents(e) {
-    let val;
-    if(e.target.classList.contains("all-clear-btn")) {
-        val = document.querySelector(".all-clear-btn").innerHTML;
-    } else if(e.target.classList.contains("number-btn")) {
-        val = document.querySelector(".number-btn").innerHTML;
+function allClear() {
+    currentItem.innerHTML = "";
+    previousItem.innerHTML = "";
+}
+
+function deleteBtn() {
+    //checking if node is empty, return if true to avoid console error
+    if(currentItem.firstChild === null) {
+        return;
+    } else {
+    currentItem.removeChild(currentItem.lastChild);
     }
-    console.log(val);
+}
+
+function equal() {
+    console.log("This is equal");
+}
+
+function numbers(e) {
+    let currentNumber = this.textContent;
+    let value = stringToHTML(currentNumber);
+    currentItem.appendChild(value);
+}
+
+function operators() {
+    let currentOperator = this.textContent;
+    let value = stringToHTML(currentOperator);
+    currentItem.appendChild(value);
+}
+
+function stringToHTML(str) {
+    let newNode = new DOMParser().parseFromString(str, 'text/html');
+    return newNode.body.firstChild;
 }
 
 // daily 1 hour coding :)
-/* accomplished aug 6,
-   tried adding event listener on all buttons
-   figured out that you need to select unique button type
+/* accomplished aug 7,
+   take textcontent/innerhtml of each button when pressed in succession - basically fixed event listener
+   managed to convert string into node which fixed appending problem
 */
-/*  aug 7 goal
-    take textcontent/innerhtml of each button when pressed in succession - basically fix event listener
-*/
-
-/* goals
-figure out which button was selected
-append whatever value is pressed remember to convert to string
-make other functions to execute action based on what button
-cycle through button clicks
-create function for each operation
+/*  aug 8 goal
+    finish working on operators function
+    start equal function
 */
