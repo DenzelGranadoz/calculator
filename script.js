bdb1e8a0d77e18bf25ed0d372f53461791016e76
Denzel Granadoz <zelgranadoz@gmail.com>
2021-08-07 19:47:35 +0800

    updated item names to avoid confusion

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+//global variables
+let currentOperand = "";
+let previousOperand = "";
+
 //selectors
-const previousItem = document.getElementById("previous-item");
-const currentItem = document.getElementById("current-item");
+const previousItemDisplay = document.getElementById("previous-item");
+const currentItemDisplay = document.getElementById("current-item");
 const allClearButton = document.querySelector(".all-clear-btn");
 const equalButton = document.querySelector(".equal-btn");
 const deleteButton = document.querySelector(".delete-btn");
@@ -16,16 +20,16 @@ operatorButtons.forEach((operator) => operator.addEventListener("click", operato
 
 //functions
 function allClear() {
-    currentItem.innerHTML = "";
-    previousItem.innerHTML = "";
+    currentItemDisplay.innerHTML = "";
+    previousItemDisplay.innerHTML = "";
 }
 
 function deleteBtn() {
     //checking if node is empty, return if true to avoid console error
-    if(currentItem.firstChild === null) {
+    if(currentItemDisplay.firstChild === null) {
         return;
     } else {
-    currentItem.removeChild(currentItem.lastChild);
+    currentItemDisplay.removeChild(currentItemDisplay.lastChild);
     }
 }
 
@@ -36,18 +40,38 @@ function equal() {
 function numbers(e) {
     let currentNumber = this.textContent;
     let value = stringToHTML(currentNumber);
-    currentItem.appendChild(value);
+    currentItemDisplay.appendChild(value);
+    currentOperand += currentNumber;
 }
 
 function operators() {
+    currentItemDisplay.innerHTML = "";
     let currentOperator = this.textContent;
-    let value = stringToHTML(currentOperator);
-    currentItem.appendChild(value);
+    //testing out syntax I learned from wes bos videos :D
+    previousOperand += `${currentOperand}${currentOperator}`;
+    currentOperand = "";
+    console.log(currentOperand);
+    let value = stringToHTML(previousOperand);
+    previousItemDisplay.appendChild(value);
+    
 }
+// to do on operators function
+// after appending, previousOperand should only have the total value from last equation
+// work on equal function to figure it out
+/* create function
+    global variable that holds current number string
+    takes in current string in currentItem
+    add operator symbol
+    append whole thing to previous item
+*/
 
 function stringToHTML(str) {
     let newNode = new DOMParser().parseFromString(str, 'text/html');
     return newNode.body.firstChild;
+}
+
+function toString(str) {
+    return str.toString();
 }
 
 // daily 1 hour coding :)
