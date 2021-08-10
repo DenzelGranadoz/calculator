8a9241a2269d3d69db908d3aecb0b9f94139b926
Denzel Granadoz <zelgranadoz@gmail.com>
2021-08-10 12:13:25 +0800

    commit source files due to issue with VM

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+//to do - finish this by aug 15 
+//figure out second round of operation
+//figure out changing of operator
+//finish delete button
+//add window listener for keyboard input
+
 //global variables
-//int variables
 let currentOperator;
 let counter = 0;
-//string variables to easily convert to node and append
 let currentOperand = "";
 let previousOperand = "";
 let temp = "";
 let total = ""; 
-
 
 //selectors
 const previousItemDisplay = document.getElementById("previous-item");
@@ -48,50 +51,47 @@ function deleteBtn() {
 
 //to-do -> efficiently append currentoperand and total into their own divs 
 function equal() {
-    temp = currentOperand;
-
-    let value = stringToHTML(temp+" =");
+    //if counter >  1 replace previous with total
+    temp = currentOperand;                  //1
+    let value = stringToHTML(temp+" =");    //1 =
     previousItemDisplay.appendChild(value);
-
-    let tempInt = parseInt(temp);
-    let previousOperandInt = parseInt(previousOperand);
-
-    currentItemDisplay.innerHTML = "";
-
-    total += equation(currentOperator, previousOperandInt, tempInt);
-
-    currentItemDisplay.appendChild(stringToHTML(total));
+    let tempFloat = parseFloat(temp);       //1-int
+    let previousOperandFloat = parseFloat(previousOperand); //1-int
+    total += equation(currentOperator, previousOperandFloat, tempFloat); //2-int
+    
+    currentItemDisplay.innerHTML = "";  
+    currentItemDisplay.appendChild(stringToHTML(total));  //2
 }
 
 function numbers() {
-    let currentNumber = this.textContent;
-    let value = stringToHTML(currentNumber);
-    currentItemDisplay.appendChild(value);
-    currentOperand += currentNumber;
-}
+    let currentNumber = this.textContent;     //1-string
+    let value = stringToHTML(currentNumber);  //1
+    currentItemDisplay.appendChild(value);    //1childnode
+    currentOperand += currentNumber;          //1 "" 1
+}   
 
 function operators() {
     currentItemDisplay.innerHTML = "";
-    currentOperator = this.textContent;
+    currentOperator = this.textContent;       //+
     //testing out syntax I learned from wes bos videos :D
     if(counter === 0) {     
-        previousOperand += `${currentOperand} ${currentOperator} `;
+        previousOperand += `${currentOperand} ${currentOperator} `; //1 + 
     } else { //need to clear here
-    temp = previousOperand;
-    previousOperand = currentOperand;
+    //temp = previousOperand;
+    previousOperand = total; //2************************ 
+    previousItemDisplay.innerHTML = "";
+    let value1 = stringToHTML(`${previousOperand} ${currentOperator}`); //2
+    previousItemDisplay.appendChild(value1);
+    return;
+    //append this together with operator but dont clear current display
     }
-    // figure out temp value ************************ 
-    let value = stringToHTML(previousOperand);
-    previousItemDisplay.appendChild(value);
-    previousOperand = currentOperand;
-    console.log(temp);
-    console.log(previousOperand);
-    currentOperand = "";
-    // if(counter == 1) {
-    //     equation(CurrentOperator, previousOperand, CurrentOperand);
-    // }
-    //console.log(previousOperand + " " + currentOperand);
-    counter++;
+    let value = stringToHTML(previousOperand);      //1 +
+    previousItemDisplay.appendChild(value);         //1 +
+    previousOperand = currentOperand;               //1
+    console.log(temp);                              //0
+    console.log(previousOperand);                   //1
+    currentOperand = "";                            //""
+    counter++;                                      //1
 }
 //reset counter somewhere
 function equation(operator, a, b) {
@@ -128,7 +128,3 @@ function stringToHTML(str) {
     return newNode.body.firstChild;
 }
 
-//to do
-//figure out second round of operation
-//figure out changing of operator
-//finish delete button
