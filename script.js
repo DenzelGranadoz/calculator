a9ddea0af92895620eb6746b464c5033d2a2f6fa
Louise <84443588+LGZel@users.noreply.github.com>
2021-08-18 13:21:20 +0800

    Finished delete button and started keyboard input

--- script.js
+++ script.js
@@ -26,6 +26,23 @@ deleteButton.addEventListener("click", deleteBtn);
 numberButtons.forEach((number) => number.addEventListener("click", numbers));
 operatorButtons.forEach((operator) => operator.addEventListener("click", operators));
 
+//do for each maybe
+// window.addEventListener("keydown", (e) => {
+//     if(e.key >= 0 && e.key <= 9) {
+//         document.querySelector(".number-btn").click();
+//     }
+// });
+
+// function keyboardInput(e) {
+//     if(e.key >= 0 && e.key <= 9) {
+//         numbers(e.key);
+//     }
+//     if(e.key == "Backspace") {
+//         deleteBtn();
+//     }
+//     console.log(e);
+// }
+
 //functions
 function allClear() {
     currentItemDisplay.textContent = "";
@@ -38,6 +55,10 @@ function allClear() {
 }
 
 function deleteBtn() {
+    if(latestButton == "=") {
+        previousItemDisplay.textContent = " ";
+        return;
+    }
     currentItemDisplay.textContent = currentItemDisplay.textContent.toString().slice(0, -1);
     currentOperand = currentItemDisplay.textContent;
 }
@@ -52,20 +73,26 @@ function equal() {
     total = equation(currentOperator, previousOperandFloat, currentOperandFloat);
 
     currentOperand = total;
-    currentItemDisplay.textContent = total; //if total contains ".", .toFixed(2)
+    currentItemDisplay.textContent = total;
+    latestButton = this.textContent;
+    
+    // this checks if total is decimal then outputs it to two decimal places
+    // if(total % 1 != 0) {
+    //     currentItemDisplay.textContent = total.toFixed(2);
+    // }
 }
 
 function numbers() {
     if(currentItemDisplay.textContent.includes('.') && this.textContent == '.') return;
-    latestButton += this.textContent
+    latestButton += this.textContent;
     currentItemDisplay.textContent = latestButton;
     currentOperand = currentItemDisplay.textContent; 
 }
 
 function operators() {
+    // call equal function in here
+    // if previous was operator, return, else equal function
     // if(operatorCounter > 0) {
-    //     call equal function in here
-    //     if previous was operator, return, else equal function
     // }
     latestButton = "";
     currentOperator = this.textContent;
